02390415bcc009adbe3b0d11598f76178da071b6
isadoraramirez <mydemoni@hotmail.com>
2020-05-07 15:38:21 -0500

    Updates

---
+++ precache-manifest.a6d8ce8b620fd38426b604dc06cacf96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df13e2fd9d7f5dacf01686483e5f01dc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c6d4f260ce05b68f530e",
+    "url": "/portfolio/static/css/main.947ea504.chunk.css"
+  },
+  {
+    "revision": "7960bd2fac4f7f24564a",
+    "url": "/portfolio/static/js/2.e21eb8ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.e21eb8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d4f260ce05b68f530e",
+    "url": "/portfolio/static/js/main.a9a6e9c0.chunk.js"
+  },
+  {
+    "revision": "1a308c33f80b2e615c9e",
+    "url": "/portfolio/static/js/runtime-main.48ca0962.js"
+  }
+]);
